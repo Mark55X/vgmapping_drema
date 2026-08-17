5ce998a30899f8817b1bda20d996c2bc4a798f19
Mark55X <marco2002stefani@gmail.com>
2026-08-17 11:34:00 +0200

    added setuppy

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="vg_mapping_recurgs_native",
+    version="0.1.0",
+    description="Native Standalone VG-Mapping & RecurGS SE(3) implementation",
+    packages=["vg_mapping_recurgs_native"],
+    package_dir={"vg_mapping_recurgs_native": "."},
+)
